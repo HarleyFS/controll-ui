b41669040820c13cadffc3804f481904ae970f31
Harley <harley201009@hotmail.com>
2023-02-26 15:42:41 -0300

    Instalando o vue-the-mask

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import VueTheMask from "vue-the-mask";
 
 //import "./assets/main.css";
-import 'v-calendar/dist/style.css';
+import "v-calendar/dist/style.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueTheMask as any);
 
 app.mount("#app");
