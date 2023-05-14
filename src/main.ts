916cc2e51aaa166c0902f584e54bb0a7eccec83b
Harley <harley201009@hotmail.com>
2023-05-14 13:10:48 -0300

    Implementando método para corrigir formato da data.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import VueTheMask from "vue-the-mask";
 //import "./assets/main.css";
 import "v-calendar/dist/style.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import moment from "moment";
 
 const app = createApp(App);
 
@@ -16,3 +17,7 @@ app.use(router);
 app.use(VueTheMask as any);
 
 app.mount("#app");
+
+Date.prototype.toISOString = function () {
+  return moment(this).format("YYYY-MM-DDTHH:mm:ss");
+};
