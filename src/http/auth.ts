a2ebea8011445dc782f4f66cb36f2c5da98118bf
Harley <harley201009@hotmail.com>
2023-08-13 18:19:26 -0300

    Alterando url base

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 const httpClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/controll",
+  baseURL: "https://service-controll.azurewebsites.net/controll",
   headers: {
     ContentType: "application/json",
   },
--- src/http/index.ts
+++ src/http/index.ts
@@ -6,7 +6,7 @@ const store = userStore();
 const isLogged = store.token != null && store.token.length > 0;
 
 const httpClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/controll",
+  baseURL: "https://service-controll.azurewebsites.net/controll",
   headers: {
     ContentType: "application/json",
     Authorization: isLogged ? `Bearer ${store.token}` : "",
